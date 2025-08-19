a45f57092788fd49f1a2fa682c3a40211fc2ddcf
yagizerdem <yagizerdem819@gmail.com>
2025-08-19 13:48:15 +0300

    extension methods added

---
+++ CodePen/ExtensionMethods/QueryableExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq.Expressions;
+
+namespace CodePen.ExtensionMethods
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> ApplyPagination<T>(this IQueryable<T> query, int page, int pageSize)
+        {
+            return query.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        public static IQueryable<T> ApplySorting<T>(this IQueryable<T> query, bool desc, Expression<Func<T, object>> keySelector)
+        {
+            return desc ? query.OrderByDescending(keySelector) : query.OrderBy(keySelector);
+        }
+
+        public static IQueryable<T> ApplyExactMatch<T>(
+            this IQueryable<T> query,
+            Expression<Func<T, string>> propertySelector,
+            string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return query;
+
+            return query.Where(Expression.Lambda<Func<T, bool>>(
+                Expression.Equal(propertySelector.Body, Expression.Constant(value)),
+                propertySelector.Parameters));
+        }
+
+        public static IQueryable<T> ApplySubstringMatch<T>(
+            this IQueryable<T> query,
+            Expression<Func<T, string>> propertySelector,
+            string substring)
+        {
+            if (string.IsNullOrEmpty(substring))
+                return query;
+
+            var method = typeof(string).GetMethod(nameof(string.Contains), new[] { typeof(string) })!;
+            var call = Expression.Call(propertySelector.Body, method, Expression.Constant(substring));
+
+            return query.Where(Expression.Lambda<Func<T, bool>>(call, propertySelector.Parameters));
+        }
+    }
+}
---
+++ CodePen/ExtensionMethods/SignInResultExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Models.Exceptions;
+using System.Net;
+
+namespace CodePen.ExtensionMethods
+{
+    public static class SignInResultExtensions
+    {
+        public static void ThrowIfFailed(this SignInResult result, string contextMessage = "Sign-in failed")
+        {
+            if (result.Succeeded) return;
+
+            var errors = new List<string>();
+            HttpStatusCode statusCode;
+
+            if (result.IsLockedOut)
+            {
+                statusCode = HttpStatusCode.Forbidden; // 403
+                errors.Add("User is locked out.");
+            }
+            else if (result.IsNotAllowed)
+            {
+                statusCode = HttpStatusCode.Forbidden; // 403
+                errors.Add("User is not allowed to sign in (e.g. email not confirmed).");
+            }
+            else if (result.RequiresTwoFactor)
+            {
+                statusCode = HttpStatusCode.Unauthorized; // 401
+                errors.Add("Two-factor authentication is required.");
+            }
+            else
+            {
+                statusCode = HttpStatusCode.Unauthorized; // 401
+                errors.Add("Invalid username or password.");
+            }
+
+            throw new AppException(
+                message: contextMessage,
+                statusCode: statusCode,
+                errors: errors,
+                isOperational: true
+            );
+        }
+    }
+}
