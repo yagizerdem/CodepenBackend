37a33bea06a4305cefd3dd8552d20ca1f0e39c2a
yagizerdem <yagizerdem819@gmail.com>
2025-08-19 13:57:49 +0300

    app user created

---
+++ Models/Entity/ApplicationUserEntity.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entity
+{
+    internal class ApplicationUserEntity : IdentityUser
+    {
+
+        [Required]
+        public required string FirstName { get; set; }
+        [Required]
+        public required string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Required]
+        public required string Role { get; set; }
+
+
+        // base entity properties 
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public EntityStatus Status { get; set; } = EntityStatus.Active;
+    }
+}
